01fc4d36e9e04644ab706423e8253b60e2e980cb
Aline Damião <aline.isilvadamiao@gmail.com>
2023-09-02 12:54:20 -0300

    criação de 'projeto'

---
+++ projeto/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+class App extends Component {
+  render(){
+    return(
+      <h1> Formulário de Cadastro</h1 >
+    );
+  }
+
+}
+
+export default App;
